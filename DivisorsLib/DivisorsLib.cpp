ae804ca5b509b7622fb7c63a40e0b05196804dea
Payan <payanab4615_aehit@students.vizja.pl>
2025-10-28 20:03:19 +0100

    Update DivisorsLib.cpp

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,26 @@
+#pragma once
 #include <vector>
-#include "Divisors.h"
+#include <cmath>
+#include <algorithm>
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+    std::vector<unsigned long long> result;
+    for (unsigned long long i = 1; i < std::sqrt(number); ++i)
+    {
+        if (number % i == 0)
+        {
+            result.push_back(i);
+        }
+    }
+    for (unsigned long long i = std::sqrt(number); i > 0; --i)
+    {
+        if (number % i == 0)
+        {
+            result.push_back(number / i);
+        }
+    }
+    return result;
+
 }
+
